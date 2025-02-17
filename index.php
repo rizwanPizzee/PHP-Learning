490b905434d7c9339a5c6461a3c8ae07a0e65aa9
rizwanPizzee <rizwanpizzee@gmail.com>
2025-02-17 20:46:01 +0500

    Learning PHP Arithmetic Operators/Operations.

--- index.php
+++ index.php
@@ -1,24 +1,68 @@
 <?php
-$name = 'John'; //string
-echo $name; //John
+$x = 11;
+$y = 3;
 
-echo "<br>";
-//when working with Double quotes.
-echo "Hello! {$name} <br>"; // Hello John 
-echo "Hello! {$name} {$name} <br>"; //Hello John John
+//Basic Arithmetic Operators (+ - * / ** %)
+echo "Basic Arithmetic Operators (+ - * / ** %) <br>";
+//Sum
+$sum = $x + $y;
+echo "SUM: {$sum} <br>"; //14
 
-//when working with Single quote.
-echo 'Hello!' . $name; // Hello John 
-echo "<br>";
-echo 'Hello!' . $name . $name; //Hello John John
+//Subtraction
+$sub = $x - $y;
+echo "SUBTRACTION: {$sub} <br>"; //8
 
-echo "<br>";
+//Multiplication
+$mul = $x * $y;
+echo "MULTIPLICATION: {$mul} <br>"; //33
 
-$fruits = "Apple";
-$quantity = 6;
-$price = 4.99;
-$total = null;
+//Division
+$div = $x / $y;
+echo "DIVISION: {$div} <br>"; //3.666667
 
-echo "Hi! {$name} you have orderd {$quantity} {$fruits}s <br>";
-$total = $quantity * $price;
-echo "Your total is \${$total}"; //in this type of scenario there must be backword slash before $ sign (used as escape character), 
+//Power (**)
+$pow = $x ** $y;
+echo "POWER: {$pow} <br>"; //1331
+
+//Mod / Remainder (%)
+$rem = $x % $y;
+echo "REMAINDER: {$rem} <br>"; //2
+
+//Basic Increment/Decrement Operators
+echo "<br>Basic Increment/Decrement Operators <br>";
+$counter = 10;
+$counter++; //or $counter = $counter + 1; both increment $counter by 1, counter = 11;
+echo "One Plus: {$counter} <br>";
+
+$counter = 10;
+$counter--; //or $counter = $counter - 1; both decrement $counter by 1, counter = 9;
+echo "One Minus: {$counter} <br>";
+
+$counter = 10;
+$counter += 3; //or $counter = $counter + 3; both increment $counter by 3, counter = 13;
+echo "Three Plus: {$counter} <br>";
+
+$counter = 10;
+$counter -= 3; //or $counter = $counter - 3; both decrement $counter by 3, counter = 7;
+echo "Three Minus: {$counter} <br>";
+
+//Operator Precedence (Priority)
+//Check from left to right and solve the expression from left to right but with order
+//First: () within parenthesis
+//Second: ** Power
+//Third: * / %
+//Fourth: + -
+
+echo "<br>Operator Precedence<br>";
+
+$total = 1 + 2 - 3 * 4 / 5 ** 6;
+
+echo "Total: {$total}";
+
+//Explaination  
+//$total = 1 + 2 - 3 * 4 / 5 ** 6;
+//$total = 1 + 2 - 3 * 4 / 15625; (Power)
+//$total = 1 + 2 - 12 / 15625; (Multiplication)
+//$total = 1 + 2 - 0.000768; (Division)
+//$total = 3 - 0.000768; (Addition)
+//$total = 2.999232; (Subtraction)
