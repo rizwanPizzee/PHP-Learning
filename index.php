53968459ed0494a4644bb2da1f28ca89631cd06f
rizwanPizzee <rizwanpizzee@gmail.com>
2025-02-21 16:27:45 +0500

    Some PHP Math functions (Absolute, Round, Floor, Ceil, Square Root, Power, Max, Min, Pi and Random)

--- index.php
+++ index.php
@@ -1,93 +1,4 @@
-<!-- 
-$_GET, $_POST:  these are special variables , use for collecting data from HTML Forms, which then sent to
-                a php file, using 'action' attribute in <form> tag. 
-                 example: <form action="example_file.php" method="get">
-
-$_GET:Data is appended in URL
-      Not Secure
-      Data Limit
-      Better for searching in page
-
-$_POST:Data is packaged inside the body of HTTP request.
-      More Secure than $_GET
-      No Data Limit
-      Better for submitting credentials.
-
-
-$_GET Example
--->
-
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>$_GET and $_POST</title>
-</head>
-
-<body>
-    <h2>$_GET Example</h2>
-    <form action="index.php" method="get"> <!-- action="index.php" it shows where we are sending our data, right now we are sending our data to the same file-->
-        <label for="username">User Name</label><br>
-        <input type="text" name="username"><br>
-        <label for="password">Password</label><br>
-        <input type="password" name="password"><br>
-        <input type="submit" value="Log In">
-    </form>
-
-    <?php
-    //Note: $_GET and $_POST variables are like array type, they store the data as indexes like below
-    //thats why we write 'name' attribute in <input> tag
-    echo "username: {$_GET["username"]} <br>";
-    echo "Password: {$_GET["password"]} <br>";
-    ?>
-
-</body>
-
-</html>
-
-<!-- The problem while using the $_GET/ 'get method' with form is that 
-whatever we write the username and password it will be shown in the URL, which is the security risk
-like http://localhost:3000/index.php?username=Rizwan&password=Mrizwan12345
-so thats why we use this for Search results.
-
-
-$_POST Example -->
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>GET and POST</title>
-</head>
-
-<body>
-    <h2>$_POST Example</h2>
-    <form action="index.php" method="post">
-        <label for="username">User Name</label><br>
-        <input type="text" name="username"><br>
-        <label for="password">Password</label><br>
-        <input type="password" name="password"><br>
-        <input type="submit" value="Log In">
-    </form>
-
-    <?php
-    echo "username: {$_POST["username"]} <br>";
-    echo "Password: {$_POST["password"]} <br>";
-    ?>
-
-</body>
-
-</html>
-
-<!-- Whatever we write the username and password it will never be shown in the URL, 
-like http://localhost:3000/index.php/
-so thats why we use this for submitting credentials. -->
-
-<!-- Real Life Example -->
+<!-- Math Functions -->
 
 <!DOCTYPE html>
 <html lang="en">
@@ -95,24 +6,44 @@ so thats why we use this for submitting credentials. -->
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>laptop House</title>
+    <title>Document</title>
 </head>
 
 <body>
-    <h2>Laptop Example</h2>
-    <form action="index.php" method="post">
-        <label for="quantity">Quantity:</label>
-        <input type="text" name="quantity" placeholder="Enter Quantity">
+
+    <form action="" method="post">
+        <label for="num1">x:</label>
+        <input type="text" name="num1"><br>
+        <label>Note: use 'y' only for Power, Max and Min functions</label><br>
+        <label for="num2">y:</label>
+        <input type="text" name="num2"><br>
         <input type="submit" value="Total">
     </form>
     <?php
-    $quantity = $_POST['quantity'];
-    $price = 270;
-    $total = null;
-    $total = $quantity * $price;
-    echo "Your Total is: {$quantity} Laptop/s x \${$price} = \${$total}"
-    ?>
+    $x = $_POST["num1"];
+    $y = $_POST["num2"] ? $_POST["num2"] : 1; //ternary operator is used  becoz, when there is no value in y variable y should be 1.
+    $absNum = abs($x); //Absolute function (abs();) returns the value without any sign, $x =-455; result= 455
+    $roundNum = round($x); //Round Function use for convering decimal number to the nearest whole number like 1.45 = 1, 5.67 = 6.
+    $floorNum = floor($x); //Floor function convert the decimal number to the previous whole number, like 1.22 =1, 4.87 = 4.
+    $ceilNum = ceil($x); //Ceiling function convert the decimal number to the next whole number, like 1.22 =2, 4.87 = 5.
+    $sqrtNum = sqrt($x); //Square Root Function take the square root of any number like sqrt(4) = 2
+    $powNum = pow($x, $y); //Power function, use: X^y= 2^4 = 16; where x = 2 and y = 4.
+    $maxNum = max($x, $y); //Max function is use for finding the maximum value between multiple variables like x, y, z etc.
+    $minNum = min($x, $y); //Min function is use for finding the minimum value between multiple variables like x, y, z etc.
+    $piNum = pi(); //Pi Function return the value of pi constant, we can use like $total = pi() * 3; => 9.424776....
+    $randNum = rand(0, 100); //Random Function returns the random num between the argument givent to the function 
 
+    echo "Absolute = {$absNum} <br>";
+    echo "Round = {$roundNum} <br>";
+    echo "Floor = {$floorNum} <br>";
+    echo "Ceil = {$ceilNum} <br>";
+    echo "Square Root = {$sqrtNum} <br>";
+    echo "Power = {$powNum} <br>";
+    echo "Max = {$maxNum} <br>";
+    echo "Min = {$minNum} <br>";
+    echo "Pi = {$piNum} <br>";
+    echo "Random (0-100) = {$randNum} <br>";
+    ?>
 </body>
 
 </html>
