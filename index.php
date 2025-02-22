755d183c9ba8ddaeb4c04e68a946992a392f8ef2
rizwanPizzee <rizwanpizzee@gmail.com>
2025-02-22 20:29:28 +0500

    Basic PHP while loop, learning and practice

--- index.php
+++ index.php
@@ -1,31 +1,31 @@
 <!--
-    for loop: it repeat some code for certain amout of time.
+    while loop : A while loop is a control flow statement that repeatedly executes a block of code 
+                as long as a specified condition remains true. 
 
-    for(initial condition; ending condition; no. of increment/decrement){
-        code....
+                It is used when the number of iterations is not known in advance 
+                and depends on a condition.
+
+    while(condition){ //the condition must be true to execute the inner block of code
+        code...
+
+        increment/decrement condition;
     }
   -->
 <?php
 
-for ($i = 1; $i <= 10; $i++) {
-    echo "{$i}. Hello! <br>";
+$counter = 3;
+while ($counter <= 10) {
+    echo "{$counter}. Hello <br>";
+    $counter++;
 }
 
 echo "<br>";
-//Increment(by 1) example
-for ($i = 1; $i <= 10; $i++) {
-    echo "{$i} <br>";
+
+$number = 9;
+while ($number < 10) {  //it will results some time differently, becouse if the $number from rand() less than 10 it will execute again, until not greater than 9.
+    $number = rand(1, 15);
+    echo "Number: $number <br>";
 }
 
-echo "<br>";
-//Decrement(by 1) example
-for ($i = 10; $i >= 1; $i--) {
-    echo "{$i} <br>";
-}
 
-echo "<br>";
-//We can also Increment/Decrement by other than 1
-for ($i = 1; $i <= 15; $i += 2) { //increment by 2 
-    echo "{$i} <br>";
-}
 ?>
