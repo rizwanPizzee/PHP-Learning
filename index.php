1955c0c4d2d61b6e18d9fa5bcb3e2d244f7e812a
rizwanPizzee <rizwanpizzee@gmail.com>
2025-02-23 18:39:46 +0500

    PHP array and some functions of array, learning and practice

--- index.php
+++ index.php
@@ -1,31 +1,39 @@
 <!--
-    while loop : A while loop is a control flow statement that repeatedly executes a block of code 
-                as long as a specified condition remains true. 
+    Array = a "variable" which can hold more than one type of values, or multiple values of same type.
 
-                It is used when the number of iterations is not known in advance 
-                and depends on a condition.
+    array(); function is used for array data type variable
 
-    while(condition){ //the condition must be true to execute the inner block of code
-        code...
-
-        increment/decrement condition;
-    }
+    $arrayName = array(value1, value2, ... , valueN);
   -->
 <?php
+$cars = array("Land Crusier", "Fortuner", "E-Tron", "Sonata", "Range Rover");
 
-$counter = 3;
-while ($counter <= 10) {
-    echo "{$counter}. Hello <br>";
-    $counter++;
+//echo $cars; // it will give an error(Warning: Array to string conversion), because we can not directly display or use or access array.
+
+//solution 1: we can call by index
+echo $cars[2] . "<br>"; // "E-Tron";
+echo $cars[0] . "<br>"; //"Land Crusier"
+
+//solution 2: Use foreach() loop to display each value
+echo "<br><b>foreach loop <br></b>";
+foreach ($cars as $car) { //it will display each value of array.
+    echo $car . "<br>";
 }
 
-echo "<br>";
+//some other functions of array
+echo "<br><b>some other functions of array <br></b>";
 
-$number = 9;
-while ($number < 10) {  //it will results some time differently, becouse if the $number from rand() less than 10 it will execute again, until not greater than 9.
-    $number = rand(1, 15);
-    echo "Number: $number <br>";
+echo count($cars) . "<br>"; //count() , counts the length of array. like 5.
+
+//we can add some value at specific index or can change the value of a specific index value.
+$cars[0] = "F1"; // new car array be like ["F1", "Fortuner", "E-Tron", "Sonata", "Range Rover"].
+array_push($cars, "Cybertruck"); //array_push() adds the value at the end of array like // new car array be like ["F1", "Fortuner", "E-Tron", "Sonata", "Range Rover", "Cybertruck"].
+array_pop($cars); //array_pop() removes the last value of an array like // new car array be like ["F1", "Fortuner", "E-Tron", "Sonata", "Range Rover"].
+array_shift($cars); //array_shift() removes the first value of an array like // new car array be like ["Fortuner", "E-Tron", "Sonata", "Range Rover"].
+$cars = array_reverse($cars); //array_reverse(), it returns a revers array, so thats why we store it into another variable.
+
+
+foreach ($cars as $car) { //it will display each value of array.
+    echo $car . "<br>";
 }
-
-
 ?>
