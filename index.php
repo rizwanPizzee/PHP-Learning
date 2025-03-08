04ab08bb279e7fa927d0038cc13015d23d4fa3e5
rizwanPizzee <rizwanpizzee@gmail.com>
2025-03-08 08:25:47 +0500

    PHP + HTML, Session learning and practice.

--- index.php
+++ index.php
@@ -1,59 +1,62 @@
 <!-- 
-   A web cookie (often just called a cookie or HTTP cookie) is a small piece of data 
-   that a website's server sends to your web browser, which then stores it locally on your device. 
-   On subsequent visits, your browser sends the cookie back to the server, 
-   allowing the website to "remember" information about you. This capability enables 
-   a range of functions, including:
+ Session: $_SESSION superglobal variable used to store data of a user, to be used accross multiple pages.
+        A user is assigned a session-id, a session is started.
 
-    Session Management: Cookies help keep you logged in and track what you do as you navigate a site 
-                        for example, remembering the items in your shopping cart.
+        This allows us to preserve data such as user authentication status, 
+        shopping cart contents, or preferences as the user navigates through different pages.
 
-    Personalization: They store your preferences (such as language settings or layout choices) 
-                    so that your experience is tailored to you.
+        Syntax:
+        session is started with: session_start(); //should be enclosed in php tags before any code.
 
-    Tracking and Analytics: Cookies can record your browsing behavior, which is useful for 
-                            website analytics or targeted advertising.
+        session is destroyed with: session_destroy(); //should be enclosed in php tags.
 
-    Cookies come in various types:
-    Session cookies: expire once you close your browser.
-    Persistent cookies: remain on your device for a set duration or until you delete them.
+        $_SESSION SuperGlobalVariable is used for storing the session values like Id, Username, Password ect.
+        $_SESSION: is an associative array.
 
-    Syntax:(Cookies are stored as key value pairs in $_COOKIE associative array)
-
-    setcookie("$key", "$value", time() + time_in_seconds, "file_path"); //time() + time_in_seconds: this is expiry time of cookie.
-    Access:
-    $_COOKIE["$key"]; or by foreach loop.
+        header("Location: file_path.extention"); //header() funtion is used for jumping into another file.
+        
+        
+        Example Below: Login(through index.php) and Logout(through home.php).
+                        from index.php we will login and gathers the username and password, and displays on home.php from there we will logout.
+        
+        Note: from where(file) we are gathering the info, there must be session_start(); function and 
+                where we are sending(file) also there should be the session_start(); fumction.
 -->
 
 <?php
-
-setcookie("fav_food", "Biryani", time() + (86400 * 2), "/"); // 85400 seconds = 1 day , and forward slash(/) in file path indcate to the current file path, which means our data will store in current file.
-
-$fav_food = $_COOKIE['fav_food'];
-echo "Cookie 1: " . $fav_food . "<br><br>"; //Cookie 1: Biryani
-
-setcookie("fav_drink", "Chai", time() + (86400 * 3), "/");
-setcookie("fav_dessert", "Ice Cream", time() + (86400 * 4), "/");
-
-$i = 1;
-foreach ($_COOKIE as $key => $value) {
-    echo "Cookie {$i}: {$value} <br>";
-    $i++;
-}
-//display of loop
-// Cookie 1: Biryani
-// Cookie 2: Chai
-// Cookie 3: Ice Cream
-
-//Advertisement example.
-
-if (isset($fav_food)) {
-    echo "<br>Buy this Biryani! only for PKR 899.";
-} else {
-    echo "<br>I don't know about your favourite food.";
-}
-
-
-//we can check that which type of cookies current website is gathering from us.
-//browser(chrome)->developer tools->Application->Cookies (if have file Name/URL select it).
+session_start();
 ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login Page</title>
+</head>
+
+<body>
+    <h1>Login Page</h1>
+    <form action="index.php" method="post">
+        <label>Username: </label>
+        <input type="text" name="username"> <br>
+        <label>Password: </label>
+        <input type="password" name="password"><br>
+        <input type="submit" value="Login" name="login">
+    </form>
+
+    <?php
+    if (isset($_POST['login'])) {
+        if (!empty($_POST['username']) && !empty($_POST['password'])) {
+            $_SESSION['username'] = $_POST['username'];
+            $_SESSION['password'] = $_POST['password'];
+            header('Location: home.php');
+        } else {
+            echo "<br>Enter both the Username and Password.<br>";
+        }
+    }
+
+    ?>
+</body>
+
+</html>
